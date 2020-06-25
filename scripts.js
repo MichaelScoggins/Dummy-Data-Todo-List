309245f1fdbdba519c2a9d84130c2acc6a3cbf65
michaelscoggins <rhythmghosts@gmail.com>
2020-06-24 19:18:16 -0500

    errthang workin

--- scripts.js
+++ scripts.js
@@ -24,12 +24,49 @@ let arrayOfTodos = [
     console.log(arrayOfTodos)
   }
 
-  const populateTodos = () => {
-    for (i=0; i < arrayOfTodos.length; i++) {
-      let todolist = document.getElementById("todo-list")
-      let createditem = document.createElement("LI")
-      let jsonresponse = document.createTextNode(arrayOfTodos[i].title)
-      createditem.appendChild(jsonresponse)
-      todolist.appendChild(createditem) 
+  const populateTodos = () => {    
+    let todolist = document.getElementById("todo-list")
+    let fontColor = ""
+    
+    for (i = 0; i < arrayOfTodos.length; i++) {   
+            
+      if(arrayOfTodos[i].completed === true) {
+        fontColor = "green"
+      } else {
+        fontColor = "red"
+      }
+
+      let todolisttag = document.createElement("LI")
+      let useridtag = document.createElement("H2")
+      let idtag = document.createElement("H2")
+      let titletag = document.createElement("H3")
+      let completedtag = document.createElement("H2")
+      // createdLI.appendChild(todolist)
+
+      let jsonresponsetitle = document.createTextNode(arrayOfTodos[i].title)
+      titletag.appendChild(jsonresponsetitle)
+      let jsonresponseuserid = document.createTextNode(arrayOfTodos[i].userId)
+      useridtag.appendChild(jsonresponseuserid)
+      let jsonresponseid = document.createTextNode(arrayOfTodos[i].id)
+      idtag.appendChild(jsonresponseid)
+      let jsonresponsecompleted = document.createTextNode(arrayOfTodos[i].completed)
+      completedtag.appendChild(jsonresponsecompleted)
+
+      todolist.appendChild(todolisttag)
+      todolisttag.appendChild(useridtag)
+      todolisttag.appendChild(idtag)
+      todolisttag.appendChild(titletag)
+      todolisttag.appendChild(completedtag)
+
+      
+      completedtag.style.color = fontColor
+      
+       
+      // createdLI.appendChild(jsonresponsetitle)
+
     }   
   }
+
+  // const completed = () => {
+    
+  // }
